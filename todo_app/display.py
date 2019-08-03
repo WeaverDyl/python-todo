5d59b04d663310a155d1e20160cd4bcbd156aa57
dylan weaver <dweaver@umd.edu>
2019-08-03 19:33:31 -0400

    remove timeago dependency, replace with own

--- todo_app/display.py
+++ todo_app/display.py
@@ -1,6 +1,7 @@
 import os
+import math
 import shutil
-import timeago
+from datetime import datetime, timedelta
 from terminaltables import AsciiTable
 
 class Display:
@@ -38,7 +39,7 @@ class Display:
             timestamp = task['Added']
             finished = task['Finished?']
 
-            formatted_timestamp = timeago.format(timestamp, locale='en_short')
+            formatted_timestamp = self.format_time(timestamp)
             formatted_finished = self.color_message('GREEN', '✓') if finished == '1' else self.color_message('RED', 'X')
 
             task['Added'] = formatted_timestamp
@@ -49,6 +50,45 @@ class Display:
         # Color as well! yellow if due date coming up, red if passed
 
         return formatted_tasks
+
+    def format_time(self, timestamp):
+        """ Returns a nice timestamp telling the user how old a task is.
+            Returns strings such as '1d ago' """
+        timestamp_datetime = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+        curr_time = datetime.now()
+        total_time_diff = curr_time - timestamp_datetime
+
+        # Time Constants
+        SECONDS_IN_MIN = 60
+        SECONDS_IN_HOUR = 3600
+        SECONDS_IN_DAY = 86400
+        SECONDS_IN_WEEK = 604800
+        SECONDS_IN_MONTH = 2592000
+        SECONDS_IN_YEAR = 31536000
+
+        # Print out formatted time difference
+        if total_time_diff.total_seconds() < 10:
+            return f'just now'
+        if total_time_diff.total_seconds() < SECONDS_IN_MIN:
+            seconds_passed = math.floor(total_time_diff.total_seconds())
+            return f'{seconds_passed}s ago'
+        if total_time_diff.total_seconds() < SECONDS_IN_HOUR:
+            minutes_passed = math.floor(total_time_diff.total_seconds() / SECONDS_IN_MIN)
+            return f'{minutes_passed}m ago'
+        if total_time_diff.total_seconds() < SECONDS_IN_DAY:
+            hours_passed = math.floor(total_time_diff.total_seconds() / SECONDS_IN_HOUR)
+            return f'{hours_passed}h ago'
+        if total_time_diff.total_seconds() < SECONDS_IN_WEEK:
+            days_passed = math.floor(total_time_diff.total_seconds() / SECONDS_IN_DAY)
+            return f'{days_passed}d ago'
+        if total_time_diff.total_seconds() < SECONDS_IN_MONTH:
+            weeks_passed = math.floor(total_time_diff.total_seconds() / SECONDS_IN_WEEK)
+            return f'{weeks_passed}w ago'
+        if total_time_diff.total_seconds() < SECONDS_IN_YEAR:
+            months_passed = math.floor(total_time_diff.total_seconds() / SECONDS_IN_MONTH)
+            return f'{months_passed}mo ago'
+        years_passed = math.floor(total_time_diff.total_seconds() / SECONDS_IN_YEAR)
+        return f'{years_passed}yr ago'
 
     def print_task_list_formatted(self, rows):
         """ Formats the rows to a table that's printed to the terminal.
